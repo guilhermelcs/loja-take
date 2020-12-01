398f49b568690e2e760c5127ca84a0670cbd398a
Guilherme Lucas <guilherme.evandf@gmail.com>
2020-12-01 03:04:38 -0300

    Axios instalado globalmente no projeto; Usando arquivos axios sotre.js guardar estado e enviar getters da propriedade produtos; Componente ListagemProdutos refatorado para usar o produto presente no estado centralizado

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -49,57 +49,14 @@ export default {
         }
     },
     computed: {
+        produtos() {
+            return this.$store.getters.getProdutos
+        },
         produtosFiltrados() {
-            if( this.categoria )
-                return this.produtos.filter( obj => obj.categoria == this.categoria )
-            return this.produtos
+            return this.$store.getters.getProdutosDaCategoria(this.categoria)
         },
         repeatFiltrado() {
-            if( this.categoria ) 
-                return 1
-            return this.repeat
-        }
-    },
-    data() {
-        return {
-            produtos: [
-                {   
-                    id: 1,
-                    nome: 'Xampu em barra',
-                    qtd: 1,
-                    preco: '43,35', 
-                    imagem: require('../assets/images/take-prod-1.jpeg'),
-                    descricao: 'Xampu em barra limpa sem agredir os fios, elaborado a frio, também conhecido como cold. Produto 100% feito à mão, natural e vegetal. Elaborado com as técnicas milenares de saboaria e cosmetologia com manteigas, óleos vegetais e óleos essenciais.',
-                    categoria: 'xampus'
-                },
-                {   
-                    id: 2,
-                    nome: 'Sabonete em barra', 
-                    qtd: 1,
-                    preco: '26,99', 
-                    imagem: require('../assets/images/take-prod-2.jpeg'),
-                    descricao: 'Sabonete em barra elaborado no processo a frio. Limpa sem agredir e mantém a oleosidade natural da pele. Produto 100% feito à mão, natural e vegetal. Elaborado com as técnicas milenares de saboaria e cosmetologia com manteigas e óleos vegetais, óleos essenciais e argilas.',
-                    categoria: 'sabonetes'
-                },
-                {   
-                    id: 3,
-                    nome: 'Condicionador sólido', 
-                    qtd: 1,
-                    preco: '43,35', 
-                    imagem: require('../assets/images/take-prod-3.jpeg'),
-                    descricao: 'Condicionador sólido para cabelos: 100% feito à mão, natural e vegetal. Agora você tem um condicionador em barra para fazer dupla com o seu xampu natural e vegetal. Os novos condicionadores da take podem ser utilizados em todos os tipos de cabelo. Além de hidratar, nutrem, fecham as cutículas, reduzem as pontas duplas, ajudam a desembaraçar os fios e proporcionam brilho e maciez por dias.',
-                    categoria: 'condicionadores'
-                },
-                {   
-                    id: 4,
-                    nome: 'Kit Hidrata', 
-                    qtd: 1,
-                    preco: '99,90', 
-                    imagem: require('../assets/images/take-prod-5.jpeg'),
-                    descricao: 'Hidratação é o destaque dos sabonetes, shampoo, e condicionador do Kit. O kit vem com 4 barras naturais, vegetais e feitos à mão elaborados no processo a frio, também conhecido como cold. Caso algum sabonete esteja sem estoque no momento da separação do seu pedido, seu kit poderá conter 1 sabonete repetido ou substituido por outro tipo. Kit em barra: limpa sem agredir e mantém a oleosidade natural da pele. Produto 100% feito à mão, natural e vegetal. Elaborado com as técnicas milenares de saboaria e cosmetologia com manteigas e óleos vegetais, óleos essenciais e argilas.',
-                    categoria: 'kits'
-                },
-            ]
+            return this.categoria ? 1 : this.repeat
         }
     },
 }
